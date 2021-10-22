f512b6d0bde6b42f48c8348630a9e0ea9b84c090
Victor Roest <victor@xirion.net>
2021-10-22 09:27:04 +0200

    add signature tests

--- lib/zookeeper/router.ex
+++ lib/zookeeper/router.ex
@@ -13,6 +13,43 @@ defmodule Zookeeper.Router do
     send_resp(conn, 200, "world")
   end
 
-  forward "/discord", to: Zookeeper.Discord.Router
+  @pk "7b4c72bef63e5742543ee008434aaafc965bf599650cdea671d5dd57b3a45163"
+      |> String.upcase()
+      |> Base.decode16!()
 
+  def verify_signature(conn, pk \\ @pk) do
+    with [signature | _] <- get_req_header(conn, "x-signature-ed25519"),
+         [timestamp | _] <- get_req_header(conn, "x-signature-timestamp"),
+         {:ok, body, conn} <- read_body(conn),
+         {:ok, json} <- Poison.decode(body),
+         {:ok, signature} <- signature |> String.upcase() |> Base.decode16() do
+      if :enacl.sign_verify_detached(signature, timestamp <> body, pk) do
+        {:ok, conn, json}
+      else
+        {:error, 401, "invalid request signature"}
+      end
+    else
+      _ -> {:error, 400, "invalid headers or body"}
+    end
+  end
+
+  post "/discord" do
+    with {:ok, conn, json} <- verify_signature(conn) do
+      case Map.fetch!(json, "type") do
+        1 ->
+          conn
+          |> put_resp_content_type("application/json")
+          |> send_resp(200, Poison.encode!(%{"type" => 1}))
+
+        _ ->
+          conn
+      end
+    else
+      {:error, code, msg} -> send_resp(conn, code, msg)
+    end
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not Found")
+  end
 end
---
+++ test/signature_test.exs
@@ -0,0 +1,61 @@
+defmodule ZookeeperTest.SignatureTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  test "test succesful sign" do
+    timestamp = "xxx"
+    msg = ~s({"type": 1})
+
+    %{public: pk, secret: sk} = :enacl.crypto_sign_ed25519_keypair()
+
+    signature = :enacl.sign_detached(timestamp <> msg, sk) |> Base.encode16()
+
+
+    # Create a test connection
+    conn = conn(:post, "/discord", msg)
+    |> put_req_header("x-signature-ed25519", signature)
+    |> put_req_header("x-signature-timestamp", timestamp)
+
+    # Invoke the plug
+    {:ok, conn, json} = Zookeeper.Router.verify_signature(conn, pk)
+
+    assert conn.status != 401
+    assert conn.status != 400
+  end
+
+  test "test invalid sign" do
+    timestamp = "xxx"
+    msg = ~s({"type": 1})
+
+    %{public: pk, secret: sk} = :enacl.crypto_sign_ed25519_keypair()
+
+    signature = :enacl.sign_detached(timestamp <> msg, sk) |> Base.encode16()
+
+
+    # Create a test connection
+    conn = conn(:post, "/discord", ~s({"type": 1, "bogus": "data"}))
+    |> put_req_header("x-signature-ed25519", signature)
+    |> put_req_header("x-signature-timestamp", timestamp)
+
+    # Invoke the plug
+    {:error, 401, _} = Zookeeper.Router.verify_signature(conn, pk)
+  end
+
+  test "test invalid json" do
+    timestamp = "xxx"
+    msg = "non-json"
+
+    %{public: pk, secret: sk} = :enacl.crypto_sign_ed25519_keypair()
+
+    signature = :enacl.sign_detached(timestamp <> msg, sk) |> Base.encode16()
+
+
+    # Create a test connection
+    conn = conn(:post, "/discord", msg)
+    |> put_req_header("x-signature-ed25519", signature)
+    |> put_req_header("x-signature-timestamp", timestamp)
+
+    # Invoke the plug
+    {:error, 400, _} = Zookeeper.Router.verify_signature(conn, pk)
+  end
+end
